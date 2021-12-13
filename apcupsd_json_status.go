83290f0c863ee01349c61ba1da5459c2c9a01f86
Nikita Potashev <v-npotas@microsoft.com>
2021-12-13 18:48:07 +0300

    Rm missing UPS values due to migration to new UPS

--- apcupsd_json_status.go
+++ apcupsd_json_status.go
@@ -26,19 +26,11 @@ type UPSCollector struct {
 	UPSName                string    `json:"USBName"`
 	UPSMode                string    `json:"UPSMode"`
 	UPSModel               string    `json:"UPSModel"`
-	UPSLoadPercent         float64   `json:"LoadPercent"`
 	BatteryChargePercent   float64   `json:"BatteryChargePercent"`
-	LineVolts              float64   `json:"LineVolts"`
-	NominalInputVoltage    float64   `json:"NominalInputVoltage"`
 	BatteryVoltage         float64   `json:"BatteryVoltage"`
 	NominalBatteryVoltage  float64   `json:"NominalBatteryVoltage"`
-	NumberTransfers        float64   `json:"BatteryNumberTransfersTotal"`
 	BatteryTimeLeftSeconds float64   `json:"BatteryTimeLeftSeconds"`
 	BatteryTimeOnSeconds   float64   `json:"BatteryTimeOnSeconds"`
-	LastTransferOnBattery  time.Time `json:"LastTransferOnBattery"`
-	LastTransferOffBattery time.Time `json:"LastTransferOffBattery"`
-	LastSelftest           time.Time `json:"LastSelftest"`
-	NominalPowerWatts      float64   `json:"NominalPowerWatts"`
 	Status                 string    `json:"status"`
 	Error                  bool      `json:"error"`
 }
@@ -61,19 +53,11 @@ func collect() *UPSCollector {
 		UPSName:                s.UPSName,
 		UPSMode:                s.UPSMode,
 		UPSModel:               s.Model,
-		UPSLoadPercent:         s.LoadPercent,
 		BatteryChargePercent:   s.BatteryChargePercent,
-		LineVolts:              s.LineVoltage,
-		NominalInputVoltage:    s.NominalInputVoltage,
 		BatteryVoltage:         s.BatteryVoltage,
 		NominalBatteryVoltage:  s.NominalBatteryVoltage,
-		NumberTransfers:        float64(s.NumberTransfers),
 		BatteryTimeLeftSeconds: s.TimeLeft.Seconds(),
 		BatteryTimeOnSeconds:   s.TimeOnBattery.Seconds(),
-		LastTransferOnBattery:  s.XOnBattery,
-		LastTransferOffBattery: s.XOffBattery,
-		LastSelftest:           s.LastSelftest,
-		NominalPowerWatts:      float64(s.NominalPower),
 		Status:                 s.Status,
 		Error:                  false,
 	}
